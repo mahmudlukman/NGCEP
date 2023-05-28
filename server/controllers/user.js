e262dab0b83e4f54211f8f077830819d884f2f5b
Lukman Mahmud <mistamann49@gmail.com>
2023-05-28 16:59:52 +0100

    delete user controller added

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,5 @@
 import User from '../models/UserModel.js'
+import bcrypt from 'bcrypt'
 
 // GET ALL USERS
 export const getAllUsers = async (req, res) => {
@@ -13,11 +14,24 @@ export const getAllUsers = async (req, res) => {
   }
 }
 
-// GET
+// GET A SINGLE USER
 export const getUser = async (req, res) => {
   try {
     const user = await User.findById(req.params.id)
     res.status(200).json(user)
+  } catch (error) {
+    res.status(500).json({message: "Something went wrong"})
+  }
+}
+
+// UPDATE USER
+
+
+// DELETE
+export const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id)
+    res.status(200).json("User has been deleted")
   } catch (err) {
     res.status(500).json({message: "Something went wrong"})
   }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getAllUsers, getUser } from '../controllers/user.js'
+import { getAllUsers, getUser, deleteUser } from '../controllers/user.js'
 
 const router = express.Router()
 
 router.get("/", getAllUsers)
 router.get("/:id", getUser)
+router.delete("/:id", deleteUser)
 
 export default router
