25c84e688cba4e7d5b85c60510b6ef48b94b60fd
Lukman Mahmud <mistamann49@gmail.com>
2023-05-30 11:24:29 +0100

    delete product controller added

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -38,3 +38,17 @@ export const getProduct = async (req, res) => {
     res.status(404).json({ message: "Something went wrong" });
   }
 }
+
+// DELETE PRODUCT
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ message: `No product exist with id: ${id}` });
+    }
+    await Product.findByIdAndRemove(id);
+    res.json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(404).json({ message: "Something went wrong" });
+  }
+};
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import {createProduct, getProducts, getProduct} from '../controllers/product.js'
+import {createProduct, getProducts, getProduct, deleteProduct} from '../controllers/product.js'
 
 const router = express.Router()
 
 router.post("/", createProduct)
 router.get("/", getProducts)
 router.get("/:id", getProduct)
+router.delete("/:id", deleteProduct)
 
 export default router
