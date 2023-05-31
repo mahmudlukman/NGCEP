24e9b18d840d4218ace65ea4c7a57dce7e49dee2
Lukman Mahmud <mistamann49@gmail.com>
2023-05-31 14:51:50 +0100

    added navbar & sidebar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
-
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
+import { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+import { themeSettings } from './theme'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from './scenes/dashboard'
+import Layout from './scenes/layout'
+import Login from './scenes/login'
 
 function App() {
-
+  const mode = useSelector((state) => state.global.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
   return (
     <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route element={<Layout />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
